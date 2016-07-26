01bddc2990094dff392b6a2cb6d87f6906109eac
itsamandeep <itsamandeep@gmail.com>
2016-07-26 19:35:43 +0000

    all buttons working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+	root 'pages#home'
   get 'pages/about'
   get 'switch_on' => 'pages#switch_on', :as => 'switch_on'
   get 'switch_off' => 'pages#switch_off', :as => 'switch_off'
